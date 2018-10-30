9e2ebb244d8416a6a48f1b9611a4c7391ed647fa
Taras <39024875+tarikmrm@users.noreply.github.com>
2018-10-30 21:33:35 +0200

    Initial commit

---
+++ Exel_3.0/MainMenu.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace Exel_3._0
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridFormula = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridValue = new System.Windows.Forms.DataGridView();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.фаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.відкритиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.зберегтиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.проПрограмуToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridFormula)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridValue)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridFormula
+            // 
+            this.dataGridFormula.AllowUserToAddRows = false;
+            this.dataGridFormula.AllowUserToDeleteRows = false;
+            this.dataGridFormula.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridFormula.Location = new System.Drawing.Point(382, 45);
+            this.dataGridFormula.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridFormula.Name = "dataGridFormula";
+            this.dataGridFormula.RowHeadersWidth = 50;
+            this.dataGridFormula.RowTemplate.Height = 24;
+            this.dataGridFormula.Size = new System.Drawing.Size(212, 244);
+            this.dataGridFormula.TabIndex = 0;
+            this.dataGridFormula.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(382, 309);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 19);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Add Count";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(249, 298);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 19);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Add Row";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(141, 298);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 19);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Value";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridValue
+            // 
+            this.dataGridValue.AllowUserToAddRows = false;
+            this.dataGridValue.AllowUserToDeleteRows = false;
+            this.dataGridValue.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridValue.Location = new System.Drawing.Point(198, 74);
+            this.dataGridValue.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridValue.Name = "dataGridValue";
+            this.dataGridValue.RowHeadersWidth = 50;
+            this.dataGridValue.RowTemplate.Height = 24;
+            this.dataGridValue.Size = new System.Drawing.Size(180, 122);
+            this.dataGridValue.TabIndex = 4;
+            this.dataGridValue.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dataGridValue_CellBeginEdit);
+            this.dataGridValue.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridValue_CellClick);
+            this.dataGridValue.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridValue_CellEndEdit);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.фаToolStripMenuItem,
+            this.проПрограмуToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(4, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(626, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // фаToolStripMenuItem
+            // 
+            this.фаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.відкритиToolStripMenuItem,
+            this.зберегтиToolStripMenuItem});
+            this.фаToolStripMenuItem.Name = "фаToolStripMenuItem";
+            this.фаToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.фаToolStripMenuItem.Text = "Файл";
+            // 
+            // відкритиToolStripMenuItem
+            // 
+            this.відкритиToolStripMenuItem.Name = "відкритиToolStripMenuItem";
+            this.відкритиToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.відкритиToolStripMenuItem.Text = "Відкрити";
+            this.відкритиToolStripMenuItem.Click += new System.EventHandler(this.відкритиToolStripMenuItem_Click);
+            // 
+            // зберегтиToolStripMenuItem
+            // 
+            this.зберегтиToolStripMenuItem.Name = "зберегтиToolStripMenuItem";
+            this.зберегтиToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.зберегтиToolStripMenuItem.Text = "Зберегти";
+            this.зберегтиToolStripMenuItem.Click += new System.EventHandler(this.зберегтиToolStripMenuItem_Click);
+            // 
+            // проПрограмуToolStripMenuItem
+            // 
+            this.проПрограмуToolStripMenuItem.Name = "проПрограмуToolStripMenuItem";
+            this.проПрограмуToolStripMenuItem.Size = new System.Drawing.Size(99, 20);
+            this.проПрограмуToolStripMenuItem.Text = "Про програму";
+            this.проПрограмуToolStripMenuItem.Click += new System.EventHandler(this.проПрограмуToolStripMenuItem_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(249, 254);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 19);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Delete Count";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(134, 254);
+            this.button5.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(87, 19);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Delete Row";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(9, 342);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(110, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(626, 370);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridValue);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridFormula);
+            this.Controls.Add(this.menuStrip1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "MainMenu";
+            this.SizeChanged += new System.EventHandler(this.MainMenu_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridFormula)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridValue)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridFormula;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridValue;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem фаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem відкритиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem зберегтиToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem проПрограмуToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Exel_3.0Tests/ParserTests.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Exel_3._0;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exel_3._0.Tests
+{
+    [TestClass()]
+    public class ParserTests
+    {
+        [TestMethod()]
+        public void EvaluateTest_2plus2()
+        {
+            Parser parser = new Parser();
+            double rezult = parser.Evaluate("2+2");
+            double correct = 2 + 2;
+            Assert.AreEqual(rezult, correct);
+        }
+        [TestMethod()]
+        public void EvaLuateTest_15_dob_15()
+        {
+            Parser parser = new Parser();
+            double rezult = parser.Evaluate("15*15");
+            double correct = 15 * 15;
+            Assert.AreEqual(rezult, correct);
+        }
+        [TestMethod()]
+        public void EvaLuateTest_15_minus_15()
+        {
+            Parser parser = new Parser();
+            double rezult = parser.Evaluate("15-15");
+            double correct = 15 - 15;
+            Assert.AreEqual(rezult, correct);
+        }
+       
+        [TestMethod()]
+        public void EvaLuateTest_2plus2dob2()
+        {
+            Parser parser = new Parser();
+            double rezult = parser.Evaluate("2+2*2");
+            double correct = 2+2*2;
+            Assert.AreEqual(rezult, correct);
+        }
+    }
+}
